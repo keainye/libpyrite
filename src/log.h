847918bb0d5dcf1d55bff1ba1a39b95075b90ec4
pokyux <pokyux@mail.ustc.edu.cn>
2023-06-26 22:28:34 +0800

    Create log.h

---
+++ src/log.h
@@ -0,0 +1,31 @@
+#include "iostream"
+#include "string"
+
+#ifdef PRT_DISABLE_LOG
+
+namespace prt {
+void log(std::string msg) {}
+void warn(std::string msg) {}
+void panic(std::string msg) {
+  exit(1);
+}
+}  // namespace prt
+
+#else
+
+namespace prt {
+void log(std::string msg) {
+  std::printf("[prt info] %s\n", msg);
+}
+
+void warn(std::string msg) {
+  std::printf("[prt warn] %s\n", msg);
+}
+
+void panic(std::string msg) {
+  std::printf("[prt error!] %s\n", msg);
+  exit(1);
+}
+}  // namespace prt
+
+#endif
