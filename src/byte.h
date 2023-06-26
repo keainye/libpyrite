621728687a499326508884c6c3a428ef5f6c22d0
pokyux <pokyux@mail.ustc.edu.cn>
2023-06-27 00:15:29 +0800

    add: bytes to string

--- src/byte.h
+++ src/byte.h
@@ -10,6 +10,7 @@
 namespace prt {
 typedef unsigned char byte;
 typedef std::vector<byte> bytes;
+
 bytes range(bytes& src, int start, int end) {
   if (end <= start) return bytes();
   int len = end-start;
@@ -17,6 +18,21 @@ bytes range(bytes& src, int start, int end) {
   for (int i = 0; i < len; i++)
     ret[i] = src[i];
   return ret;
+}
+
+std::string to_string(bytes& src) {
+  int end = src.size();
+  for (int i = 0; i < src.size(); i++) {
+    if (!src[i]) {
+      end = i;
+      break;
+    }
+  }
+  char str[end+1];
+  for (int i = 0; i < end; i++)
+    str[i] = src[i];
+  str[end] = 0;
+  return std::string(str);
 }
 }  // namespace prt
 
