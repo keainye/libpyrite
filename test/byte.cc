4bcdbc40dcd93128f94a1aaedd525aec308519cc
pokyux <pokyux@mail.ustc.edu.cn>
2023-06-27 12:58:25 +0800

    enhance byte test

--- test/byte.cc
+++ test/byte.cc
@@ -5,10 +5,11 @@ using namespace prt;
 using namespace std;
 
 int main() {
-  bytes b(3);
+  bytes b(4);
   b[0] = '0';
   b[1] = '1';
   b[2] = '2';
+  b[3] = 0;
 
   test(to_string(b) == "012");
   pass("to_string(bytes&)");
@@ -16,11 +17,11 @@ int main() {
   test(to_bytes("012") == b);
   pass("to_bytes(std::string&)");
 
-  test(to_bytes("012")+to_bytes("345") == to_bytes("012345"));
+  test(range(b, 0, 3)+to_bytes("345") == to_bytes("012345"));
   pass("bytes& operator+(const bytes&, const bytes&)");
 
   b = to_bytes("012345");
-  test(range(b, 3, 6) == to_bytes("345"));
+  test(range(b, 3, 7) == to_bytes("345"));
   pass("bytes range(bytes&, int, int)");
 
   return 0;
