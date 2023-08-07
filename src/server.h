174316e9c337af0718dbccdcd3130e961ecf689b
pokyux <pokyux@mail.ustc.edu.cn>
2023-08-07 16:29:13 +0800

    init server class

--- src/server.h
+++ src/server.h
@@ -6,13 +6,24 @@
 #include "mocutils/channel.h"
 #include "package.h"
 #include "winsock2.h"
+#include "functional"
 
 namespace prt {
 class client_data {
+public:
   sockaddr_in addr;
   i64 last_acpt;
   i32 seq;
   std::map<i32, moc::channel<package>> promises;
+};
+
+class server {
+  struct sockaddr_in addr;
+  int port;
+  i64 max_lifetime;
+  std::map<std::string, std::function<bytes(bytes)>> router;
+  i64 timeout;
+  std::map<sockaddr_in, client_data> clients;
 };
 };  // namespace prt
 
