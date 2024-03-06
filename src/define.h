5a92692d3b3c2584ba7c9bc5147a772891e0987b
pokyux <pokyux@mail.ustc.edu.cn>
2024-03-06 22:18:53 +0800

    migrate byte from mocutils

--- src/define.h
+++ src/define.h
@@ -1,11 +1,15 @@
 #ifndef _PRT_DEFINE_H
 #define _PRT_DEFINE_H
 
+#include "mocutils/byte.h"
+
 typedef long i32;
 typedef long long i64;
 
 namespace prt {
   const int max_transmit_size = 1024;
+  typedef moc::byte byte;
+  typedef moc::bytes bytes;
 }
 
 #endif
