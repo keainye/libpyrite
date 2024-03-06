469a642766a30991a04e3c23491d442231b70683
pokyux <pokyux@mail.ustc.edu.cn>
2024-03-06 22:25:11 +0800

    add definition

--- src/server.h
+++ src/server.h
@@ -9,28 +9,13 @@
 #include "winsock2.h"
 
 namespace prt {
-// class client_data {
-//  public:
-//   sockaddr_in addr;
-//   i64 last_acpt;
-//   i32 seq;
-//   std::map<i32, moc::channel<package>> promises;
-// };
-
 class server {
-  struct sockaddr_in addr;
-  int port;
-  i64 max_lifetime;
-  std::map<std::string, std::function<bytes(sockaddr_in, bytes)>> router;
-  i64 timeout;
-  // std::map<sockaddr_in, client_data> clients;
-
  public:
-  server(int _port, i64 _max_lifetime, i64 _timeout);
+  server(int port, i64 timeout);
   void start();
-  void process(sockaddr_in client, package recv);
   bool add_router(std::string identifier, std::function<bytes(sockaddr_in, bytes)> ctrler);
   void tell(sockaddr_in client, std::string identifier, bytes body);
+  bytes promise(sockaddr_in client, std::string identifer, bytes body);
 };
 };  // namespace prt
 
