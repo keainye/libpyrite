a44da657dafae88bd0493e8a9554835b6d8e51eb
pokyux <pokyux@mail.ustc.edu.cn>
2024-03-10 10:17:06 +0800

    fix log

--- src/log.h
+++ src/log.h
@@ -1,35 +1,8 @@
 #ifndef _PRT_LOG
 #define _PRT_LOG
 
-#include "iostream"
-#include "string"
+void log(std::string msg);
+void warn(std::string msg);
+void panic(std::string msg);
 
-#ifdef PRT_DISABLE_LOG
-
-namespace prt {
-void log(std::string msg) {}
-void warn(std::string msg) {}
-void panic(std::string msg) {
-  exit(1);
-}
-}  // namespace prt
-
-#else
-
-namespace prt {
-void log(std::string msg) {
-  std::printf("[prt info] %s\n", msg);
-}
-
-void warn(std::string msg) {
-  std::printf("[prt warn] %s\n", msg);
-}
-
-void panic(std::string msg) {
-  std::printf("[prt error!] %s\n", msg);
-  exit(1);
-}
-}  // namespace prt
-
-#endif
 #endif
