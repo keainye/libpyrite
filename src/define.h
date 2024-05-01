830c374aea3371de5cdf288aee7b673093db400b
pokyux <pokyux@mail.ustc.edu.cn>
2024-05-01 21:46:03 +0800

    noresp

--- src/define.h
+++ src/define.h
@@ -8,6 +8,8 @@ typedef long long i64;
 
 #define assert(expr) if (!expr) exit(1);
 
+#define PRT_NORESP moc::bytes()
+
 namespace prt {
   const int max_transmit_size = 1024;
   typedef moc::byte byte;
